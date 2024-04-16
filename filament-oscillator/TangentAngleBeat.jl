f691eb06be67fc7eaa58a5a8acc4138d5a3cbe32
Pablo Zurita Soler <pzurita@uc.cl>
2024-04-16 15:23:25 +0100

    Reformulation of elasticity in the model to be linear increase in angle deviation

--- filament-oscillator/TangentAngleBeat.jl
+++ filament-oscillator/TangentAngleBeat.jl
@@ -18,19 +18,12 @@ T_rec = T - T_eff
 w = f_w*L
 c = (L + w)/T_rec
 
-function θ(s::Real, t::Real)
-    period_t = mod(t, T)
-    if period_t < T_eff
-        return θ_eff(period_t)
-    else
-        return θ_rec(s, period_t - T_eff)
-    end
-end
 
-function θ_eff(t::Real)
-    return θ_0*cos(π*t/T_eff)
-end
+"""
+    g(u::Real)
 
+Returns the value of the transition function `g` for a given argument `u`.
+"""
 function g(u::Real)
     if u ≤ -0.5
         return -1.0
@@ -43,17 +36,16 @@ function g(u::Real)
     end
 end
 
-function θ_rec(s::Real, t::Real)
-    return θ_0*((1.0 - f_ψ)*sin(π*t/T_rec - π/2.0) - f_ψ*g(
-        (s - c*t)/w + 0.5
-    ))
-end
+"""
+    ∫(a::Real, b::Real, f::Function, method::String)
 
+Numerically integrates the function `f` on the domain (`a`, `b`) with method `method`.
+"""
 function ∫(a::Real, b::Real, f::Function, method::String)
     if b - a < eps()
         return 0.0
     end
-    if method == "gauss"
+    if method == "gausslegendre"
         function fˢ(v::Real)
             u = (b - a)*v/2.0 + (a + b)/2.0
             return 2.0/(b - a)*f(u)
@@ -66,102 +58,127 @@ function ∫(a::Real, b::Real, f::Function, method::String)
     end
 end
 
-function tangent_at_t(t::Real)
-    function tan_x(s::Real)
-        return cos(θ(s, t) + orientation)
-    end
-    function tan_z(s::Real)
-        return sin(θ(s, t) + orientation)    
-    end
-    return tan_x, tan_z
-end
+"""
+    θ(s::Real, ψ_1::Real)
 
-function ξ_time(s::Real, t::Real)
-    tan_x, tan_z = tangent_at_t(t)
-    if s < eps()
-        return [0., 0., 0.]
-    end
-    x_position = ∫(0., s, tan_x, "trapezoidal")
-    z_position = ∫(0., s, tan_z, "trapezoidal")
-    return [x_position, 0., z_position]
-end
-
-function ξ_time(s::Vector, t::Real)
-    tan_x, tan_z = tangent_at_t(t)
-    N, = size(s)
-    return L/N*[cumsum(tan_x.(s)), 0., cumsum(tan_z.(s))]
-end
-
-function ξ(s::Real, ψ_1::Real)
-    t = T*ψ_1/(2.0*π)
-    return ξ_time(s, t)
-end
-
-function ξ(s::Vector, ψ_1::Real)
-    t = T*ψ_1/(2.0*π)
-    return ξ_time(s, t)
-end
-
-function dθ_dt(s::Real, t::Real)
-    period_t = mod(t, T)
-    if period_t < T_eff
-        return dθ_eff_dt(period_t)
+Returns the preffered tangent angle for the given arclength `s` and shape phase `ψ_1`.
+"""
+function θ(s::Real, ψ_1::Real)
+    mod_ψ_1 = mod2pi(ψ_1)
+    if mod_ψ_1 < f_eff*2π
+        return θ_eff(mod_ψ_1)
     else
-        return dθ_rec_dt(s, period_t - T_eff)
+        return θ_rec(s, mod_ψ_1 - f_eff*2π)
     end
 end
 
-function dθ_eff_dt(t::Real)
-    return -θ_0*sin(π*t/T_eff)*π/T_eff
+"""
+    θ_eff(ψ_1::Real)
+
+Returns the angle of the filament during the effective stroke at shape phase `ψ_1`.
+"""
+function θ_eff(ψ_1::Real)
+    return θ_0*cos(ψ_1/(2.0*f_eff))
 end
 
-function dg_dx(u::Real)
-    if u ≤ -0.5 || u ≥ 0.5
-        return 0.0
+"""
+    θ_rec(s::Real, ψ_1::Real)
+
+Returns the tangent angle of the filament during the recovery stroke at arclength `s` and
+shape phase `ψ_1`.
+"""
+function θ_rec(s::Real, ψ_1::Real)
+    return θ_0*((1.0 - f_ψ)*sin(ψ_1/(2.0*f_rec) - π/2.0) - f_ψ*g(
+        (s - (c*T/2π)*ψ_1)/w + 0.5
+    ))
+end
+
+"""
+    ∂θ_∂ψ_1(s::Real, ψ_1::Real)
+
+Returns the derivative of the tangent angle of the filament with respect to `ψ_1` at
+arclength `s` and shape phase `ψ_1`.
+"""
+function ∂θ_∂ψ_1(s::Real, ψ_1::Real)
+    mod_ψ_1 = mod2pi(ψ_1)
+    if mod_ψ_1 < f_eff*2π
+        return ∂θ_eff_∂ψ_1(mod_ψ_1)
     else
-        return 4.0*(4.0*u^2 + 1.0)*sech(4.0*u/(4.0*u^2 - 1.0))^2/((1.0 - 4.0*u^2)^2)
+        return ∂θ_rec_∂ψ_1(s, mod_ψ_1 - f_eff*2π)
     end
 end
 
-function dθ_rec_dt(s::Real, t::Real)
-    first_term = (1.0 - f_ψ)*cos(π*t/T_rec + π/2.0)*π/T_rec
-    second_term = f_ψ*dg_dx((s - c*t)/w + 1.0/2.0)*c/w
+"""
+    ∂θ_eff_∂ψ_1(ψ_1::Real)
+
+Returns the derivative of the tangent angle of the filament during the effective stroke
+at shape phase ψ_1.
+"""
+function ∂θ_eff_∂ψ_1(ψ_1::Real)
+   return -θ_0*sin(ψ_1/(2.0*f_eff))/(2.0*f_eff) 
+end
+
+"""
+    ∂θ_rec_∂ψ_1(s::Real, ψ_1::Real)
+
+Returns the derivative of the tangent angle of the filament during the recovery stroke
+with respect to `ψ_1` at arclength `s` and shape phase `ψ_1`.
+"""
+function ∂θ_rec_∂ψ_1(s::Real, ψ_1::Real)
+    first_term = (1.0 - f_ψ)*cos(ψ_1/(2.0*f_rec) + π/2.0)/(2.0*f_rec)
+    second_term = f_ψ*dg_dx((s - (c*T/2π)*ψ_1)/w + 1.0/2.0)*(c*T/2π)/w
     return θ_0*(first_term + second_term)
 end
 
-function normal_at_t(t::Real)
-    function n_x(s::Real)
-        return -sin(θ(s, t) + orientation)
+"""
+    ξ(s::Real, ψ::Vector)
+
+Returns the position of the filament at arglenth `s` and phase `ψ`.
+"""
+function ξ(s::Real, ψ::Vector)
+    function x_integrand(σ::Real)
+        return cos(θ_bend(σ, ψ[1]) + ψ[2]*s/L)
     end
-    function n_z(s::Real)
-        return cos(θ(s, t) + orientation)
+    function z_integrand(σ::Real)
+        return sin(θ_bend(σ, ψ[1]) + ψ[2]*s/L)
     end
-    return n_x, n_z
+    x = ∫(0.0, s, x_integrand, "gausslegendre")
+    z = ∫(0.0, s, z_integrand, "gausslegendre")
+    return [x, 0.0, z]
 end
 
-function dθ_dt_at_t(t::Real)
-    function dθ_dt_arclength(s::Real)
-        return dθ_dt(s, t)
+"""
+    ∂ξ_∂ψ_1(s::Real, ψ::Vector)
+
+Returns the derivative of the position of the filament at arglenth `s` and phase `ψ` with
+respect to `ψ_1`.
+"""
+function ∂ξ_∂ψ_1(s::Real, ψ::Vector)
+    function x_integrand(σ::Real)
+        return -sin(θ_bend(σ, ψ[1]) + ψ[2]*s/L)*∂θ_∂ψ_1(σ, ψ[1])
     end
-    return dθ_dt_arclength
+    function z_integrand(σ::Real)
+        return cos(θ_bend(σ, ψ[1]) + ψ[2]*s/L)*∂θ_∂ψ_1(σ, ψ[1])
+    end
+    x = ∫(0.0, s, x_integrand, "gausslegendre")
+    z = ∫(0.0, s, z_integrand, "gausslegendre")
+    return [x, 0.0, z]
 end
 
-function dξ_dψ_1_time(s::Real, t::Real)
-    jacobian = T/(2.0*π)
-    dθ = dθ_dt_at_t(t)
-    n_x, n_z = normal_at_t(t)
-    function integrand_x(s_prime::Real)
-        return n_x(s_prime)*dθ(s_prime)*jacobian
-    end
-    function integrand_z(s_prime::Real)
-        return n_z(s_prime)*dθ(s_prime)*jacobian
-    end
-    x_position = ∫(0., s, integrand_x, "trapezoidal")
-    z_position = ∫(0., s, integrand_z, "trapezoidal")
-    return [x_position, 0., z_position]
-end
+"""
+    ∂ξ_∂ψ_2(s::Real, ψ::Vector)
 
-function dξ_dψ_1(s::Real, ψ_1::Real)
-    t = T*ψ_1/(2.0*π)
-    return dξ_dψ_1_time(s, t)
+Returns the derivative of the position of the filament at arglenth `s` and phase `ψ` with
+respect to `ψ_2`.
+"""
+function ∂ξ_∂ψ_2(s::Real, ψ::Vector)
+    function x_integrand(σ::Real)
+        return -sin(θ_bend(σ, ψ[1]) + ψ[2]*s/L)*s/L
+    end
+    function z_integrand(σ::Real)
+        return cos(θ_bend(σ, ψ[1]) + ψ[2]*s/L)*s/L
+    end 
+    x = ∫(0.0, s, x_integrand, "gausslegendre")
+    z = ∫(0.0, s, z_integrand, "gausslegendre")
+    return [x, 0.0, z]
 end
