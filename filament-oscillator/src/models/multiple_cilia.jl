88a623601feb7240318ceb5f2d03dc7a292995fc
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-07 14:04:42 +0100

    better modularisation

--- filament-oscillator/src/models/multiple_cilia.jl
+++ filament-oscillator/src/models/multiple_cilia.jl
@@ -159,40 +159,7 @@ function run_system(p::NamedTuple, final_time::Real, num_steps::Int, alg)
     t_span = (0.0, final_time)
     Ψ₀ = repeat([1.0, 0.0]; outer=[M])
     problem = ODEProblem(filament_oscillators!, Ψ₀, t_span, p)
-    solution = solve(problem, alg)#, dt=final_time/num_steps)
+    solution = solve(problem, alg, dt=final_time/num_steps)
     return solution
 end
 
-function plot_system(solution::ODESolution)
-    Ψ_array = stack(solution.u, dims=1)[begin:size(solution.u)[1]÷100 + 1:end, :]
-    p = plot(
-        xlim=(-L*1.1, L*1.1), ylim=(0.0, L*1.1), title="System movement",
-        xaxis="x [μm]", yaxis="z [μm]", legend=false
-    )
-    xlims!(p, (-L*1.1, (M - 1)*d + L*1.1))
-    color_scheme = palette(:twilight, size(Ψ_array)[1], rev=true)
-    @gif for k = 1:size(Ψ_array)[1]
-        x_vector = x(Ψ_array[k, :])
-        for j=1:M
-            positions = zeros(N+1, 3)
-            positions[1, :] = x₀[j]
-                for i=1:N
-                positions[i+1, :] += x_vector[i + (j - 1)N, :]
-            end
-            plot!(p, positions[:, 1], positions[:, 3], color=color_scheme[k])
-        end
-    end
-    display(p)
-    return nothing
-end
-
-function plot_system_trajectory(solution::ODESolution)
-    Ψ_array = stack(solution.u, dims=1)
-    t = solution.t
-    p = plot(title="Phase evolution", xaxis="t [ms]")
-    for i=1:size(Ψ_array)[2]
-        plot!(t, Ψ_array[:, i], label="ψ_$i")
-    end
-    display(p)
-    return nothing
-end
---
+++ filament-oscillator/src/visualisation/plotters.jl
@@ -0,0 +1,45 @@
+using Plots
+
+"""
+    animate_cilia(solution::ODESolution)
+
+Animates the cilia movement given the solution `solution` of the ODE.
+"""
+function animate_cilia(solution::ODESolution)
+    Ψ_array = stack(solution.u, dims=1)[begin:size(solution.u)[1]÷100 + 1:end, :]
+    p = plot(
+        xlim=(-L*1.1, L*1.1), ylim=(0.0, L*1.1), title="System movement",
+        xaxis="x [μm]", yaxis="z [μm]", legend=false
+    )
+    xlims!(p, (-L*1.1, (M - 1)*d + L*1.1))
+    color_scheme = palette(:twilight, size(Ψ_array)[1], rev=true)
+    @gif for k = 1:size(Ψ_array)[1]
+        x_vector = x(Ψ_array[k, :])
+        for j=1:M
+            positions = zeros(N+1, 3)
+            positions[1, :] = x₀[j]
+                for i=1:N
+                positions[i+1, :] += x_vector[i + (j - 1)N, :]
+            end
+            plot!(p, positions[:, 1], positions[:, 3], color=color_scheme[k])
+        end
+    end
+    display(p)
+    return nothing
+end
+
+"""
+    plot_system_trajectory(solution::ODESolution)
+
+Plots the phase evolution of the system given the solution `solution` of the ODE.
+"""
+function plot_system_trajectory(solution::ODESolution)
+    Ψ_array = stack(solution.u, dims=1)
+    t = solution.t
+    p = plot(title="Phase evolution", xaxis="t [ms]")
+    for i=1:size(Ψ_array)[2]
+        plot!(t, Ψ_array[:, i], label="ψ_$i")
+    end
+    display(p)
+    return nothing
+end
