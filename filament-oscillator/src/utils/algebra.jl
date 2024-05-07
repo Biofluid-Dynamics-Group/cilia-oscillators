13e1916f5d353d328d2790183e921407a160bff7
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-07 15:34:27 +0100

    moved purely algebraic utility function here

---
+++ filament-oscillator/src/utils/algebra.jl
@@ -0,0 +1,20 @@
+using LinearAlgebra
+using Base.Threads
+
+
+"""
+    block_diagonal(matrices::Array{Matrix})
+
+Given an array of equally-sized matrices, returns a block-diagonal matrix with the
+matrices of the array on the diagonal.
+"""
+function block_diagonal(matrices::Vector)
+    example = matrices[1]
+    rows, columns = size(example)
+    n = length(matrices)
+    result = zeros(rows*n, columns*n)
+    @threads for i = 1:n
+        result[1 + (i - 1)*rows:i*rows, 1 + (i - 1)*columns:i*columns] .= matrices[i]
+    end
+    return result
+end
