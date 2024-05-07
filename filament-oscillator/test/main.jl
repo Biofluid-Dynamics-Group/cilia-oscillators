21dbdb4afe77ac1c39b459fc914ba50aa7241fe5
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-07 16:02:15 +0100

    setup main file to run

--- filament-oscillator/test/main.jl
+++ filament-oscillator/test/main.jl
@@ -1,20 +1,37 @@
-include("../models/multiple_cilia.jl")
+include("../src/models/cilia.jl")
+include("../src/visualisation/plotters.jl")
 
 
 # Physical parameters
-a = 7e-2  # μm
-μ = 1.0  # Pa s
-κ_b = 1.0  # pN μm^2
-T = 60  # ms
+a = 7e-2  # μm, sphere radius
+μ = 1.0  # Pa s, fluid viscosity
+κ_b = 1.0  # pN μm^2, bending stiffness
+
+# Geometric setup
+M = 1  # Number of cilia
+d = 40.0  # μm, distance between cilia
+φ = 12.5*π/180.0  # rad, angle of cilia beat plane
+
+# Discretisation parameters
+N = 5  # Number of discretisation spheres per cilium
+
+# Initial phase
+Ψ₀ = repeat([2π, 0.0], outer=[M])
 
 # Simulation parameters
-num_steps = 500
-num_periods = 2
+num_periods = 1
 alg = radau()
 
-# Run the system
-p = (a=a, μ=μ, κ_b=κ_b)
-solution = run_system(p, num_periods*T, num_steps, alg)
+# Instance objects
+h = L/(N - 1)
+system = CiliaSystem(
+    params = CiliaParameters(M, N, φ, d, a, κ_b),
+    x₀ = [[(j - 1)*d, 0.0, 0.0] for j=1:M],
+    s = [(i)*h for i=1:N]
+    A = [cos(φ) -sin(φ) 0.0; sin(φ) cos(φ) 0.0; 0.0 0.0 1.0],
+    Ψ = Ψ₀
+)
+fluid = FluidParameters(μ)
 
-# Plot the system trajectory
-plot_system_trajectory(solution)
+# Run the system
+solution = run_system(system, fluid, num_periods*T, alg)
