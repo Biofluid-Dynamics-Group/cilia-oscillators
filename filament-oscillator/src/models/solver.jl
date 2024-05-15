c4c3fd9ff26c646f9f8dcc96eac419a669f42d78
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-15 20:13:53 +0100

    debugging and adding velocity and fixed timestepping functionality

--- filament-oscillator/src/models/solver.jl
+++ filament-oscillator/src/models/solver.jl
@@ -11,21 +11,29 @@ Returns the phase velocity at a given system phase. The phase velocity is determ
 solving the saddle-point system that serves as the equation of motion for the system of
 filaments.
 """
-function Ψ_dot(system::CiliaSystem, fluid::FluidParameters)
+function Ψ_dot(system::CiliaSystem, fluid::FluidParameters, t::Real)
     Q = Q_ref(system, fluid)
-    κ_matrix = κₕ(system)
+    κ_matrix = -κₕ(system)
     matrix = [
-        Πₕ(system, fluid) -κ_matrix;
-        -κ_matrix' zeros(2system.params.M, 2system.params.M)
+        Πₕ(system, fluid) κ_matrix;
+        κ_matrix' zeros(2system.params.M, 2system.params.M)
     ]
     elastic_relaxation = cat(
-        [[0.0, system.params.κ_b*system.Ψ[2j]] for j=1:system.params.M]..., dims=1
+        [[0.0, -system.params.κ_b*system.Ψ[2j]] for j=1:system.params.M]..., dims=1
     )
+    u = repeat([-1.0, 0.0, 0.0]*sin(ω*t), outer=[system.params.M*system.params.N])
+    # system_matrix = inv(κ_matrix'*inv(Πₕ(system, fluid))*κ_matrix)
+    # println("eigenvalues of matrix", eigen(system_matrix).values)
+    # println("....")
     force = Q + elastic_relaxation
-    rhs = vcat(zeros(system.params.M*3system.params.N), -force)
+    # rhs = vcat(zeros(system.params.M*3system.params.N), -force)
+    rhs = vcat(u, -force)
     problem = LinearProblem(matrix, rhs)
     solution = solve(problem, KrylovJL_GMRES())
     dΨ_dt = solution[end-(2*system.params.M - 1):end]
+    println("t: ", t)
+    println("Ψ: ", system.Ψ)
+    println("dΨ_dt: ", dΨ_dt)
     return dΨ_dt
 end
 
@@ -40,7 +48,7 @@ function filament_oscillators!(dΨ_dt::Vector, Ψ::Vector, p::NamedTuple, t::Rea
     # The solver tells the system its new phase
     p.system.Ψ .= Ψ
     # The system tells the solver the new phase velocities given the current phase
-    dΨ_dt .= Ψ_dot(system, p.fluid)
+    dΨ_dt .= Ψ_dot(system, p.fluid, t)
     return nothing
 end
 
@@ -61,7 +69,8 @@ function run_system(
     p = (system=system, fluid=fluid)
     problem = ODEProblem(filament_oscillators!, Ψ₀, t_span, p)
     if num_steps != 0
-        solution = solve(problem, alg=alg, dt=time/num_steps)
+        solution = solve(
+            problem, alg=alg, adaptive=false, dt=time/num_steps, reltol=1e-3, abstol=1e-3)
     else
         solution = solve(problem, alg=alg)
     end
