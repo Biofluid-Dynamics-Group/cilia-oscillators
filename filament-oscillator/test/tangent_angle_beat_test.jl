186d65b5c96a9653e5db43cc3ff0a8a03c2e8bd5
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-17 17:20:17 +0100

    not real test, but file to check if beat is working

--- filament-oscillator/test/tangent_angle_beat_test.jl
+++ filament-oscillator/test/tangent_angle_beat_test.jl
@@ -4,15 +4,25 @@ include("../src/beats/tangent_angle.jl")
 
 
 ψ₁_array = collect(0.0:0.1:4π)
-ψ₂_array = collect(0.0:0.1:2π)
+ψ₂_array = collect(-π:0.1:π)
 s_array = collect(0.0:0.1:20.0)
 
 p = plot(xlims=(-20.0, 20.0), ylims=(0.0, 20.0), legend=false)
-for ψ₁ in ψ₁_array
+@gif for ψ₁ in ψ₁_array
     positions = zeros(length(s_array), 3)
     for (i, s) in enumerate(s_array)
         positions[i, :] .= ξ(s, [ψ₁, 0.0])
     end
-    plot!(p, positions[:, 1], positions[:, 3])
+    plot(p, positions[:, 1], positions[:, 3])
+end
+display(p)
+
+p = plot(xlims=(-20.0, 20.0), ylims=(-20.0, 20.0), legend=false)
+@gif for ψ₂ in ψ₂_array
+    positions = zeros(length(s_array), 3)
+    for (i, s) in enumerate(s_array)
+        positions[i, :] .= ξ(s, [π*f_eff, ψ₂])
+    end
+    plot(p, positions[:, 1], positions[:, 3])
 end
 display(p)
