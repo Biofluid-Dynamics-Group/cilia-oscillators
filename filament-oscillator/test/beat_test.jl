d270226b545ad2df84e438873bf852d299db9151
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-17 19:22:22 +0100

    visualizer for beats

--- filament-oscillator/test/beat_test.jl
+++ filament-oscillator/test/beat_test.jl
@@ -3,21 +3,21 @@ using Plots
 include("../src/beats/tangent_angle.jl")
 
 
-ψ₁_array = collect(0.0:0.1:4π)
+ψ₁_array = collect(0.0:0.1:2π)
 ψ₂_array = collect(-π:0.1:π)
-s_array = collect(0.0:0.1:20.0)
+s_array = collect(0.0:0.1:L)
 
-p = plot(xlims=(-20.0, 20.0), ylims=(0.0, 20.0), legend=false)
+p = plot(xlims=(-L, L), ylims=(0.0, L), legend=false)
 @gif for ψ₁ in ψ₁_array
     positions = zeros(length(s_array), 3)
     for (i, s) in enumerate(s_array)
         positions[i, :] .= ξ(s, [ψ₁, 0.0])
     end
-    plot(p, positions[:, 1], positions[:, 3])
+    plot!(p, positions[:, 1], positions[:, 3])
 end
 display(p)
 
-p = plot(xlims=(-20.0, 20.0), ylims=(-20.0, 20.0), legend=false)
+p = plot(xlims=(-L, 20.0), ylims=(-L, L), legend=false)
 @gif for ψ₂ in ψ₂_array
     positions = zeros(length(s_array), 3)
     for (i, s) in enumerate(s_array)
