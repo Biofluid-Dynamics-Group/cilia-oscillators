7effda9170b02655fdf8916e2b653429682a2c8e
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-17 14:57:25 +0100

    changed u to x

---
+++ filament-oscillator/test/tangent_angle_beat_test.jl
@@ -0,0 +1,18 @@
+using Plots
+
+include("../src/beats/tangent_angle.jl")
+
+
+ψ₁_array = collect(0.0:0.1:4π)
+ψ₂_array = collect(0.0:0.1:2π)
+s_array = collect(0.0:0.1:20.0)
+
+p = plot(xlims=(-20.0, 20.0), ylims=(0.0, 20.0), legend=false)
+for ψ₁ in ψ₁_array
+    positions = zeros(length(s_array), 3)
+    for (i, s) in enumerate(s_array)
+        positions[i, :] .= ξ(s, [ψ₁, 0.0])
+    end
+    plot!(p, positions[:, 1], positions[:, 3])
+end
+display(p)
