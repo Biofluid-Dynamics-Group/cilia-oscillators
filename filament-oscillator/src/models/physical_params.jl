78c963a6a7ef12c85a58759efcfdb066f5527d2a
Pablo Zurita Soler <pzurita@uc.cl>
2024-05-28 12:34:10 +0100

    better params

--- filament-oscillator/src/models/physical_params.jl
+++ filament-oscillator/src/models/physical_params.jl
@@ -1,40 +1,28 @@
+# All lengths are given divided by a
+# All times are given divided by T
 
 
 # Single cilium geomerty
 a = 1.0  # Sphere radius, reference
 N = 5  # Number of discretisation spheres per cilium
 sphere_space = 0.2  # Fraction of radii between spheres
+L = N*(2 + sphere_space)*a  # Cilium length w/r to sphere radius
 
 # Cilia system geomerty
 M = 2  # Number of cilia
-d_tilde = 50  # Amount of radii between cilia
+d = 50  # Amount of radii between cilia
 φ = 0 # 12.5*π/180.0  # Angle of cilia beat plane
 
 # Single cilium reference beat dynamics
 T = 1.0  # Beat period, reference
 θ_0 = π/2.1  # Maximum bending angle
 f_eff = 0.3  # Effective stroke fraction
-f_rec = 1.0 - f_eff  # Recovery stroke fraction
 f_ψ = 0.85  # Fraction of recovery controlled by travelling wave
 f_w = 0.4  # Travelling wavelength w/r to cilium length
 orientation = π/2.0  # Normal to attachment plane
 
 # Fluid dynamics
 μ = 1.0  # Fluid viscosity, reference
-κ_tilde = 0.3  # Ratio of bending stiffness to viscous drag
+κ_tilde = 1.2  # Ratio of bending stiffness to viscous drag
+κ_b = κ_tilde*μ*T  # bending stiffness
 
--
-###########################################################################################
-
-
-# Physical parameters
-L = N*(2 + sphere_space)*a  # Cilium length w/r to sphere radius
-d = d_tilde*a  # Distance between cilia w/r to sphere radius
-
-ω = 2π/T  # Beat frequency
-κ_b = κ_tilde*μ*ω*L^4  # bending stiffness
-
-T_eff = T*f_eff  # Effective stroke length
-T_rec = T*f_rec  # Recovery stroke length
-w = f_w*L  # Travelling wavelength
-c = (L + w)/T_rec  # Travelling wave speed
