59e7c6663ca618690b831000b1565b05fba12701
Pablo Zurita Soler <pzurita@uc.cl>
2024-06-04 11:37:36 +0100

    updated tests

--- filament-oscillator/test/beat_test.jl
+++ filament-oscillator/test/beat_test.jl
@@ -1,3 +1,6 @@
+include("../src/utils/integral.jl")
+using .Integral
+using ProgressMeter
 using Plots
 
 include("../src/beats/tangent_angle.jl")
@@ -16,21 +19,21 @@ beat_params = BeatParameters(L, T, θ_0, f_eff, f_ψ, f_w, orientation, rule)
 ψ₂_array = collect(-π:0.1:π)
 s_array = collect(0.0:0.1:L)
 
-# p = plot(xlims=(-L, L), ylims=(0.0, L), legend=false)
-# @gif for ψ₁ in ψ₁_array
-#     positions = zeros(length(s_array), 3)
-#     for (i, s) in enumerate(s_array)
-#         positions[i, :] .= ξ(s, [ψ₁, 0.0], beat_params)
-#     end
-#     plot!(p, positions[:, 1], positions[:, 3])
-# end
-
-p = plot(xlims=(-L, 20.0), ylims=(-L, L), legend=false)
-@gif for ψ₂ in ψ₂_array
+p = plot(xlims=(-L, L), ylims=(0.0, L), legend=false)
+@gif for ψ₁ in ψ₁_array
     positions = zeros(length(s_array), 3)
     for (i, s) in enumerate(s_array)
-        positions[i, :] .= ξ(s, [π*f_eff, ψ₂], beat_params)
+        positions[i, :] .= ξ(s, [ψ₁, 0.0], beat_params)
     end
-    plot(p, positions[:, 1], positions[:, 3])
+    plot!(p, positions[:, 1], positions[:, 3])
 end
-display(p)
+
+# p = plot(xlims=(-L, 20.0), ylims=(-L, L), legend=false)
+# @gif for ψ₂ in ψ₂_array
+#     positions = zeros(length(s_array), 3)
+#     for (i, s) in enumerate(s_array)
+#         positions[i, :] .= ξ(s, [π*f_eff, ψ₂], beat_params)
+#     end
+#     plot(p, positions[:, 1], positions[:, 3])
+# end
+# display(p)
--- filament-oscillator/test/derivative_test.jl
+++ filament-oscillator/test/derivative_test.jl
@@ -1,20 +1,26 @@
+include("../src/utils/integral.jl")
+using .Integral
+using ProgressMeter
 using Test
 
 include("../src/beats/tangent_angle.jl")
+include("../src/models/physical_params.jl")
 
 rule = GaussLegendre(140)
+params = BeatParameters(L, T, θ_0, f_eff, f_ψ, f_w, orientation, rule)
 
 @testset "Derivative of θ_rec" begin
     for s = 0.0:1:L
-        ∂θ_at_s(ψ) = ∂θ_rec_∂ψ_1(s, ψ)
-        for ψ₁ = 0.0:0.5:2π*f_rec
-            @test ∫(0.0, ψ₁, ∂θ_at_s, rule) ≈ θ_rec(s, ψ₁) - θ_rec(s, 0.0) atol=1e-6
+        ∂θ_at_s(ψ) = ∂θ_rec_∂ψ_1(s, ψ, params)
+        for ψ₁ = 0.0:0.5:2π*params.f_rec
+            @test ∫(0.0, ψ₁, ∂θ_at_s, rule) ≈ θ_rec(s, ψ₁, params) - θ_rec(s, 0.0, params) atol=1e-6
         end
     end
 end
 
 @testset "Derivative of θ_eff" begin
-    for ψ₁ = 0.0:0.1:2π*f_eff
-        @test ∫(0.0, ψ₁, ∂θ_eff_∂ψ_1, rule) ≈ θ_eff(ψ₁) - θ_eff(0.0) atol=1e-6
+    for ψ₁ = 0.0:0.1:2π*params.f_eff
+        ∂θ_no_params(ψ) = ∂θ_eff_∂ψ_1(ψ, params)
+        @test ∫(0.0, ψ₁, ∂θ_no_params, rule) ≈ θ_eff(ψ₁, params) - θ_eff(0.0, params) atol=1e-6
     end
 end
